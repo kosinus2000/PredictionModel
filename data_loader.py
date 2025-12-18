9f12d074f6f46b85381836e3c34239cae3845a89
Hubert <hubert.kosiorowski1@gmail.com>
2025-12-18 15:09:23 +0100

    d

---
+++ data_loader.py
@@ -0,0 +1,18 @@
+﻿from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+    with tarfile.open(tarball_path) as tar:
+        tar.extractall(path="datasets")
+
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+housing_data = load_housing_data()
